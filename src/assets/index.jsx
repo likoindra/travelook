a6803a3160674410efa21f8297d96ac3daf4e967
it devpermata <itdevpermata@its-MacBook-Air-2.local>
2022-07-26 13:53:12 +0700

    inital commit

---
+++ src/assets/index.jsx
@@ -0,0 +1,23 @@
+export { default as TRAVELOOK_LOGO } from "./Logo.png";
+
+export { default as BANNER_LOGO } from "./BannerMain.png";
+export { default as HOMEPAGE_LOGO_1} from "./Home1.png";
+export { default as HOMEPAGE_LOGO_2 } from "./Home2.png";
+export { default as HOMEPAGE_LOGO_3 } from "./Home3.png";
+
+export { default as DETAIL_PRODUCT_IMAGE } from './DetailProduct.png';
+export { default as CHECKOUT_IMAGE } from './CheckoutImage.png';
+export { default as LOGIN_ICON} from './LoginIcon.png';
+
+export { default as FOOTER_LOGO_1 } from './mamikos.png';
+export { default as FOOTER_LOGO_2 } from './rd.png';
+export { default as FOOTER_LOGO_3 } from './oyo.png';
+export { default as FOOTER_LOGO_4 } from './rd.png';
+export { default as FOOTER_LOGO_5 } from './airy.png';
+export { default as FOOTER_LOGO_6 } from './bobobox.png';
+
+
+
+
+
+
